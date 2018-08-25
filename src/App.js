b2559c99dff3771da71fa93c7432cab619a0b61a
fashmuyhee <fashmuyidavid@gmail.com>
2018-08-24 17:31:01 -0700

    Secoond commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Service from "./pages/service";
 import Contact from "./pages/contact";
-import User from "./components/user";
+import UserDetail from "./pages/user_detail";
+import UserSettings from "./pages/user_settings";
 import Error from "./pages/404";
 import SignIn from "./pages/auth/sign-in";
 import SignUp from "./pages/auth/sign-up";
@@ -20,13 +21,14 @@ class App extends Component {
           <Route path="/service" component={Service} />
           <Route path="/contact" component={Contact} />
 
-          {/*<Route path="/user/:token" component={UserDetail} />*/}
-          {/* <User /> */}
 
           <Route path="/sign-in" component={SignIn} />
           <Route path="/sign-up" component={SignUp} />
 
-          <Route path="/user" component={User} />
+          {/* <User /> */}
+          <Route path="/user/:token" component={UserDetail} />
+          {/*<Route path="/user" component={UserDetail} />*/}
+          <Route path="/user_settings" component={UserSettings} />
 
           <Route component={Error} />
         </Switch>
--- src/pages/user_settings.js
+++ src/pages/user_settings.js
@@ -1,26 +1,62 @@
 import React,{Component} from 'react';
-import Footer from '../components/footer'
-import Navbar from '../components/navbar'
+import User from '../components/user'
 
 
 class UserSettings extends Component {
 
    render() {
 	return(
-	    <div>
-		 <div>
-		    <Navbar/>
-		 </div>
-		 <div className="wrapper">
-		    <div className="page-header page-header-xs" data-parallax="true" style={{"backgroundImage": `url(${require('../images/profileBg.jpg')})`}} >
-			 <div className="filter"/>
-		    </div>
-		    Settings
-		    <div>
-			 <Footer/>
-		    </div>
-		 </div>
-	    </div>
+	   <User>
+		<div>
+		   <form>
+			<div className="row">
+
+			   <div className="col-md-6">
+				<div className="form-group">
+				   <input type="text" className="form-control" placeholder="Username" aria-describedby="basic-addon1" name="username"/>
+				</div>
+			   </div>
+
+			   <div className="col-md-6">
+				<div className="form-group">
+				   <input type="email" className="form-control" placeholder="Email" aria-describedby="basic-addon1" name="email"/>
+				</div>
+			   </div>
+		   	</div>
+
+			<div className="row">
+			   <div className="col-md-6">
+				<div className="form-group">
+				   <input type="text" className="form-control" placeholder="Description" aria-describedby="basic-addon1" name="description"/>
+				</div>
+			   </div>
+
+			   <div className="col-md-6">
+				<div className="form-group">
+				   <input type="number" className="form-control" placeholder="Phone number" aria-describedby="basic-addon1" name="phone"/>
+				</div>
+			   </div>
+			</div>
+			<div className="row">
+			   <div className="col-md-12">
+				<div className="form-group">
+				   <textarea className="form-control" rows="4" placeholder="Biography"/>
+				</div>
+			   </div>
+			</div>
+			<div className="row">
+			   <div className="col-md-12">
+				<div className="form-group">
+				   <textarea className="form-control" rows="4" placeholder="Address"/>
+				</div>
+			   </div>
+			</div>
+			<div className="center">
+			   <button className="btn btn-success btn-icon btn-round">Save <i className="nc-icon nc-check-2"/> </button>
+			</div>
+		   </form>
+		</div>
+	   </User>
 
 	)
    }
